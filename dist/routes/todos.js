a642f065e412051a8a9bb5e13612b6ae6f4a0b5d
Pierre <pierre.lagadec@gmail.com>
2022-01-18 11:59:23 +1100

    improving project folder structure

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -7,9 +7,10 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
     todos.push(newTodo);
     res.status(201).json({
@@ -18,19 +19,22 @@ router.post('/todo', (req, res, next) => {
     });
 });
 router.put('/todo/:todoId', (req, res, next) => {
-    const id = req.params.todoId;
+    const body = req.body;
+    const params = req.params;
+    const id = params.todoId;
     const todoIndex = todos.findIndex(item => item.id === id);
     if (todoIndex >= 0) {
         todos[todoIndex] = {
             id: todos[todoIndex].id,
-            text: req.body.text
+            text: body.text
         };
         return res.status(200).json({ message: 'updated todo!' });
     }
     res.status(404).json({ message: 'update error: could not find todo for this id' });
 });
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter(item => item.id !== req.params.todoId);
+    const params = req.params;
+    todos = todos.filter(item => item.id !== params.todoId);
     return res.status(200).json({ message: 'deleted todo!' });
 });
 exports.default = router;
