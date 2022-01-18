72c65499e01cfc2a4a5c19cb4ca7eb39abfff27c
Pierre <pierre.lagadec@gmail.com>
2022-01-18 11:28:29 +1100

    adding a PUT route

--- routes/todos.ts
+++ routes/todos.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { Todo } from '../models/todo';
 
-
 const todos: Todo[] = [];
 
 const router = Router();
@@ -15,6 +14,24 @@ router.post('/todo', (req, res, next) => {
         id: new Date().toISOString(),
         text: req.body.text
     };
+    todos.push(newTodo);
+    res.status(201).json({ 
+        message: 'added todo!',
+        todo: newTodo
+     });
+});
+
+router.put('/todo/:todoId', (req, res, next) => {
+    const id = req.params.todoId;
+    const todoIndex = todos.findIndex(item => item.id === id);
+    if(todoIndex >= 0) {
+        todos[todoIndex] = {
+            id: todos[todoIndex].id,
+            text: req.body.text
+        };        
+        return res.status(200).json({ message: 'updated todo!' });
+    }
+    res.status(404).json({ message: 'could not find todo for this id' });
 });
 
 export default router;
